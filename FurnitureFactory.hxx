795764cb716b8314f37dbf207b97aaa6c2cdd7f0
Enhope Estheim <Enhope.Estheim@gmail.com>
2020-05-23 03:14:40 +0200

    feat: The interface for the furniture factory class.

---
+++ FurnitureFactory.hxx
@@ -0,0 +1,39 @@
+//-----------------------------------------------------------------------------
+// File:   FurnitureFactory.hxx
+// Author: Enhope
+// Date:   23-05-2020
+//-----------------------------------------------------------------------------
+#ifndef FURNITUREFACTORY_HXX_
+#define FURNITUREFACTORY_HXX_
+
+#include <abstract_factory_pattern/Furniture.hxx>
+//-----------------------------------------------------------------------------
+// \brief Factory class.
+class Factory
+{
+public:
+    //! \brief Virtual destructor for subclasses.
+    virtual ~Factory();
+
+    virtual Furniture::FurniPtr createChair() = 0;
+    virtual Furniture::FurniPtr createTable() = 0;
+    virtual Furniture::FurniPtr createBed() = 0;
+};
+
+//-----------------------------------------------------------------------------
+// \brief Furniture Factory class.
+class FurnitureFactory
+{
+public:
+    //! \brief Constructor.
+    FurnitureFactory();
+
+    //! \brief Virtual destructor for subclasses.
+    virtual ~FurnitureFactory();
+
+    virtual Furniture::FurniPtr createChair() = 0;
+    virtual Furniture::FurniPtr createTable() = 0;
+    virtual Furniture::FurniPtr createBed() = 0;
+};
+
+#endif /* FURNITUREFACTORY_HXX_ */
